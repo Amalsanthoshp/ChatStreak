33088237e74ec1b554d86780a5e99c949821fa0e
amalsanthoshp <amaldemaniax@gmail.com>
2019-11-27 19:35:33 +0530

    bootstrap add

--- chat-ui/src/Chat.js
+++ chat-ui/src/Chat.js
@@ -13,10 +13,31 @@ class Chat extends React.Component {
 	render(){
 		return ( 
 			<>
-			<div className="container">  
-			  <div className='side-left'>1</div>  
-			  <div className='main'><ChatScreen/></div>  
-			  <div className='side-right'>3</div>  
+			<div className="container-fluid">  
+			  <div className="row">
+			    <div className="col-md-2">
+			    	<p className='text-center'> 1 div </p>
+			    	<div className="toast" role="alert" aria-live="assertive" aria-atomic="true">
+					  <div className="toast-header">
+					    <img src="..." className="rounded mr-2" alt="..."/>
+					    <strong className="mr-auto">Bootstrap</strong>
+					    <small className="text-muted">11 mins ago</small>
+					    <button type="button" className="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
+					      <span aria-hidden="true">&times;</span>
+					    </button>
+					  </div>
+					  <div className="toast-body">
+					    Hello, world! This is a toast message.
+					  </div>
+					</div>
+			    </div>
+			    <div className="col-md-8">
+			    	<ChatScreen/>
+			    </div>
+			    <div className="col-md-2">
+			      <p className='text-center'> 3 div </p>
+			    </div>
+			    </div> 
 			 </div>				
 			</>
 
--- chat-ui/src/Components/Chat-screen.js
+++ chat-ui/src/Components/Chat-screen.js
@@ -11,13 +11,29 @@ class ChatScreen extends React.Component {
 		return(
 
 			<div>
-			 <div style={{background:'white'}}><h3> name</h3></div>
-			  <div style={{background:'wheat',height:'80vh'}}>
-				<div className='bottom-mess'> 
-
-					<input type='text'/><p>a</p>
+				<div className="card" style={{height:'100vh'}}>
+				  <div className="card-body">
+				  	<div>
+				  		<nav className="nav">
+						  <a className="nav-link active" href="#">Active</a>
+						  <a className="nav-link" href="#">Link</a>
+						  <a className="nav-link" href="#">Link</a>
+						  <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+						</nav>
+				  	</div>
+				  	<div id='cont'> 
+				  	<p> hello</p>
+				  	</div>
+				    <div className="input-group input-group-md bttm">
+					  <div className="input-group-prepend">
+					    <span className="input-group-text" id="inputGroup-sizing-md">File</span>
+					  </div>
+					  <input type="text" className="form-control inp" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-md"/>
+					  <button type="button" className="btn-sm btn-success ">Send</button>
+					</div>
+				  </div>
 				</div>
-			  </div>
+			 
 			</div>
 
 
