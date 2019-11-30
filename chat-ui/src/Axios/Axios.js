cfde45441b59e4bbf6521fe0a2382c4d2cceb278
amalsanthoshp <amaldemaniax@gmail.com>
2019-11-30 18:02:45 +0530

    functions for axios

---
+++ chat-ui/src/Axios/Axios.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import axios from 'axios';
+
+
+export function postMessage(para1,para2,para3){
+	axios.defaults.xsrfHeaderName = "X-CSRFToken";
+		axios.defaults.xsrfCookieName = "csrftoken";
+	    function getCookie(name) {
+		    var cookieValue = null;
+		    if (document.cookie && document.cookie !== '') {
+		        var cookies = document.cookie.split(';');
+		        for (var i = 0; i < cookies.length; i++) {
+		            var cookie = cookies[i].trim();
+		            // Does this cookie string begin with the name we want?
+		            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+		                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+		                break;
+		            }
+		        }
+		    }
+		    return cookieValue;
+		}
+		var csrftoken = getCookie('csrftoken');
+		console.log(csrftoken)
+	  	  axios.post(`http://localhost:8000/message_send/`, 
+
+	  	  		{
+		            'message':para1,
+		            'send_to':para2,
+		            'test':para3,
+		        },
+		        {
+		            headers: {
+		                "Content-Type": 'application/json',
+		                'X-CSRFTOKEN': csrftoken,
+
+					
+
+		            },
+		        }
+		    )
+		    .then(res => console.log(res))
+		    .catch(error => console.log(error))
+}
+
+
+
