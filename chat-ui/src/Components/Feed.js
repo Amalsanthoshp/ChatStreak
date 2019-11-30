dfb81b0fa360b0c875ed2f4f0a8163c6f8c1d6ef
amalsanthoshp <amaldemaniax@gmail.com>
2019-11-30 17:59:30 +0530

    this.props for Feed element

--- chat-ui/src/Components/Feed.js
+++ chat-ui/src/Components/Feed.js
@@ -7,21 +7,25 @@ class Feed extends React.Component {
 		super()
 
 	}
+	componentDidUpdate(prevProps) {
+		  if (this.props.id !== prevProps.id) {
+		    this.fetchData(this.props.id);
+		  }
+		}
 	render(){
 
 		return(
-			<>
-					  
+
+			<>	  
 				<div className="ui fluid grey card" style={{marginTop:'.5rem'}}>
 					<div className="right floated author" style={{paddingTop:'.5rem'}}>
-					      <img className="ui avatar image" src="https://cbsnews1.cbsistatic.com/hub/i/2016/03/23/38e32f54-b910-4612-8852-be9e0fbdbf73/cat-istock.jpg"/> <b>{this.props.message_props.username} </b>
+					      <img className="ui avatar image" src={this.props.image}/> <b>{this.props.name}</b>
 					    </div>
 					  <div className="content">
-					  	<span className='left floated'> {this.props.message_props.message_send ? this.props.message_props.message_send[0].message:null}</span>
-					  	<span className="left floated time description">{this.props.message_props.message_send ? this.props.message_props.message_send[0].sent_time:null}</span> 
+					  	<span className='left floated'> {this.props.message}</span>
+					  	<span className="left floated time description">{this.props.time}</span> 
 					</div>
-					</div>
-					 
+					</div>		 
             </>
 
 
