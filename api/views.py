82050b7bdbec699104d9f583599092b51c769e71
amalsanthoshp <amaldemaniax@gmail.com>
2019-11-30 17:55:07 +0530

    api view for message-creation

--- api/views.py
+++ api/views.py
@@ -4,6 +4,10 @@ from .models import *
 from .serializers import *
 from rest_framework import generics
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
+import json
+from django.utils import timezone
 
 
 # Create your views here.
@@ -32,9 +36,13 @@ class ListGroup(generics.ListCreateAPIView):
 	
 @csrf_exempt 
 def message_send(request):
-	if request.method == "POST": #os request.GET()
-		mess = request.POST.get("hellos")
-		print(mess)
-		return HttpResponse(mess)
-	else:
-		return HttpResponse('hello')
+	body_unicode = request.body.decode('utf-8')
+	body = json.loads(body_unicode)
+	message = body['message']
+	send_to = body['send_to']
+	now = timezone.now()
+	send_id = Person.objects.get(id=1)
+	recevied_id = Person.objects.get(id=2)
+	if message:
+		queryset = Chat.objects.create(user_sent=send_id,user_recevied=recevied_id,message=message,sent_time=now)
+	return HttpResponse(message)
