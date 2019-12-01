371b5412de45416df8903a761fb911579e663283
amalsanthoshp <amaldemaniax@gmail.com>
2019-12-01 11:50:15 +0530

    icon for delete

--- chat-ui/src/Components/Feed.js
+++ chat-ui/src/Components/Feed.js
@@ -24,7 +24,22 @@ class Feed extends React.Component {
 					  <div className="content">
 					  	<span className='left floated'> {this.props.message}</span>
 					  	<span className="left floated time description">{this.props.time}</span> 
-					</div>
+					  	<div className="ui icon bottom right pointing dropdown button">
+						  <i className="wrench icon"></i>
+						  <div className="menu">
+						    <div className="item">
+						      <i className="dropdown icon"></i>
+						      <span className="text">New</span>
+						      <div className="menu">
+						        <div className="item">Document</div>
+						        <div className="item">Image</div>
+						      </div>
+						    </div>
+						    <div className="item">Save As...</div>
+						    <div className="item">Edit</div>
+						  </div>
+						</div>
+					  </div>
 					</div>		 
             </>
 
