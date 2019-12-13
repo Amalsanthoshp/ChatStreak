ff5adf39211cde35e30377bb8ea4f19d978473be
amalsanthoshp <sebastien@ixxo.io>
2019-12-13 18:47:23 +0530

    menu for recent chats

---
+++ chat-ui/src/Components/Recent.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import IndividualChat from './Indivdual-chat';
+
+class Recent extends React.Component {
+
+
+
+	render(){
+		if(this.props.chat){
+			console.log(this.props.chat)
+			let numberOfMessage =this.props.chat.length;
+			console.log(numberOfMessage)
+		 	var rows = [];
+		 	for (var i = 0; i < numberOfMessage ; i++) {
+				    rows.push(<span key={i} className='item'>
+				    	<IndividualChat
+				    	 id = {this.props.chat[i].id}
+				    	 name= {this.props.chat[i].user_sent_id}
+				    	 message={this.props.chat[i].message}
+				    	 time={this.props.chat[i].time}
+				    	 /></span>);
+				    }
+		 }
+		 else{
+		 	let rows = <div> Loading....</div>
+		 }
+
+		return (
+				 <div className="ui list fluid" style={{overflow:'scroll',height:'36vh'}}>
+				    {rows}
+				  </div>
+
+
+
+			)
+	}
+
+}
+
+export default Recent;
