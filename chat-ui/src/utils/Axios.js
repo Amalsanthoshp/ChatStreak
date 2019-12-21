39a7da17dc648860533f8e6a6aaf2b35c6174fb3
amalsanthoshp <amaldemaniax@gmail.com>
2019-12-21 14:47:12 +0530

    new login way

--- chat-ui/src/utils/Axios.js
+++ chat-ui/src/utils/Axios.js
@@ -196,8 +196,56 @@ export function getRecent(url){
 
 
 export function logOut(url){
+		 axios.get('http://localhost:8000/api-auth/logout/')
 		 localStorage.setItem('token', null);
 		 cogoToast.success('Logged Out Successfully 👋',{color:'green',hideAfter:2});
 		 history.replace('/login');   
 }
 
+
+
+
+export function getFeed(url){
+	axios.defaults.xsrfHeaderName = "X-CSRFToken";
+		axios.defaults.xsrfCookieName = "csrftoken";
+	    function getCookie(name) {
+		    var cookieValue = null;
+		    if (document.cookie && document.cookie !== '') {
+		        var cookies = document.cookie.split(';');
+		        for (var i = 0; i < cookies.length; i++) {
+		            var cookie = cookies[i].trim();
+		            // Does this cookie string begin with the name we want?
+		            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+		                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+		                break;
+		            }
+		        }
+		    }
+		    return cookieValue;
+		}
+		var csrftoken = getCookie('csrftoken');
+		console.log(csrftoken)
+		let Token = localStorage.getItem('token')
+		var config = {
+		            
+		    headers: {'Authorization' : "JWT " + Token}
+		};		            		                
+
+	  	return axios.get(url,config)
+		    .then(function (res){
+		    	if (res.status=='200'){
+		    	console.log(res);
+		     	console.log('user selected')
+		     	return res
+
+		     }})
+		    .catch(function (error) {
+			    if (error.response) {
+			      if(error.response.status =='400' || error.response.status =='401'){
+			      	return error
+			      }
+			    }
+			  })
+		    
+
+}
