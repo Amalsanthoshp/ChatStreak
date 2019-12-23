b0f4b2dca9ed832341e1e4ca73c63cdd3df483c9
amalsanthoshp <amaldemaniax@gmail.com>
2019-12-23 18:03:37 +0530

    onclick event added

--- chat-ui/src/Components/Indivdual-chat.js
+++ chat-ui/src/Components/Indivdual-chat.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 
 class IndividualChat extends React.Component {
@@ -6,19 +7,31 @@ class IndividualChat extends React.Component {
 	constructor(){
 		super()
 
+
+
+	this.handleFeed = this.handleFeed.bind(this);
 	}
+	
+	handleFeed(){
+		this.props.feed(this.props.id)
+
+	}
+	
 	render(){
 
 		return(
 				<>
-					<div className="ui green card" style={{borderRadius:'0!important'}}>
-					<div className="right floated author">
-					      <img className="ui avatar image" src="https://semantic-ui.com/images/avatar/small/jenny.jpg"/> {this.props.name}
-					    </div>
-					  <div className="content" style={{paddingBottom:'0',paddingTop:'5px'}}>
-					  	<span className='left floated'> {this.props.message}</span>
-					  	<span className="right floated time">{this.props.time}</span> 
-					</div>
+			   	    <div id={this.props.id} className="ui green card" style={{borderRadius:'0!important'}} onClick={this.handleFeed}>
+				 	   <div className="right floated author">
+					 	 <div>
+					       <img className="ui avatar image" src="https://semantic-ui.com/images/avatar/small/jenny.jpg"/> {this.props.name}<br></br>
+					       <span className=''> {this.props.message}</span>
+
+					      </div>
+					     </div>
+					    <div style={{paddingBottom:'0',paddingTop:'5px'}}>
+					  	 <span className="right floated time">{this.props.time}</span> 
+					  </div>
 					</div>
 				</>
 			);
