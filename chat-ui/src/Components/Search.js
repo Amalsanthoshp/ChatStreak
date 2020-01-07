d78f2101f67e7457a756a5f343b593fdfd19c6e7
amal-san <amaldemaniax@gmail.com>
2020-01-07 12:27:10 +0530

    search with no user message

--- chat-ui/src/Components/Search.js
+++ chat-ui/src/Components/Search.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import * as Axios from '../utils/Axios';
+
 
 
 export default class Search extends React.Component {
@@ -7,24 +9,98 @@ export default class Search extends React.Component {
 
 		super()
 
+		this.state = {
+
+			users:[],
+
+		}
+
+	    this.handleChange = this.handleChange.bind(this);
 	    this.handleClick = this.handleClick.bind(this);
+	    this.handleSelect = this.handleSelect.bind(this);
+	}
+
+	handleSelect(event) {
+
+		var id = event.target.id
+		this.props.searchSelect(id.split('_')[0])
+		
 	}
 
 
 	handleClick() {
-		alert('hello')
+
+		document.getElementById('search').style.display = 'none';
+		document.getElementById('search-results').style.display = 'none';
+	}
+
+	handleChange() {
+
+
+		if(document.getElementById('search').value===''){
+					document.getElementById('search-results').style.display = 'none!important';
+
+		}
+
+		let baseUrl = 'http://localhost:8000/api/search/'
+		let url = baseUrl + document.getElementById('search').value
+		Axios.userSearch(url)
+		.then(res => {
+			const users = res.data;
+	        this.setState({ users })
+	       	console.log(this.state.users)
+		
+		 })	
+		.catch((error) => {
+		      console.log(error);
+		      this.setState({ users:null })
+
+
+		});
+	  
 	}
 
 	render() {
+
+			if(this.state.users){
+				let numberOfUsers =this.state.users.length;
+				var rows = [];
+		 	 for (var i = 0; i < numberOfUsers ; i++) {
+		 	 		rows.push(
+		 	 		<a onClick={this.handleSelect} key={i} id ={this.state.users[i].id+'_selectItem'} className="result">
+			         <div className="content">
+			          <div id ={this.state.users[i].id+'_select'} className="title">{this.state.users[i].username}</div>
+			         </div>
+			        </a>);
+			    }
+		 		
+			}
+
+			
+
+			let row = this.state.users ? rows : <a className="result">
+			   									  <div className="content">
+			          							  <div className="title">No users..</div>
+			         							 </div>
+										        </a>
+
 		return(
 
-			<div className="ui category right alinged search" onClick={this.handleClick}>
+			
+
+			<div className="ui category right alinged search">
 			  <div className="ui icon input">
-			    <input className="prompt" type="text" placeholder="Search animals..." id='search' style={{display:'none',transition:'opacity 2s ease-out',opacity:'0'}}/>
-			    <i className="search icon"></i>
+			    <input className="prompt" type="text" placeholder="Search Friends..." id='search' onChange={this.handleChange} style={{display:'none',transition:'opacity 2s ease-out',opacity:'0'}}/>
+			    <i className="search icon" onClick={this.handleClick}></i>
 			  </div>
-			  <div className="results"></div>
-			</div>
+			  <div className="results transition" id='search-results' style={{display:'none',width:'150px'}}>
+			    <div className="category">
+			        <div className="results">
+			        {row}
+			      </div>
+			     </div>
+			    </div>
+			   </div>
 
 
 
