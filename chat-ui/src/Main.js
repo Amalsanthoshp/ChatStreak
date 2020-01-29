3888c97a8e1c17887a41780891fc10aad8c01806
amal-san <amaldemaniax@gmail.com>
2020-01-29 15:05:54 +0530

    container padding

--- chat-ui/src/Main.js
+++ chat-ui/src/Main.js
@@ -12,7 +12,7 @@ class Main extends Component {
   render() {
     return (
       <Router history={history}>
-        <div className="ui one item menu" style={{background:'transparent'}}>
+        <div className="ui one item menu" style={{background:'transparent',marginBottom:'0'}}>
 		  <div className="item" style={{justifyContent:'center'}}> <span role="img" style={{fontSize:'1.5rem'}} aria-label="fire">🔥</span> Streakchat</div>
 		</div>
           {/*<ul className="header">
@@ -20,7 +20,7 @@ class Main extends Component {
             <li><a href="/stuff">Stuff</a></li>
             <li><a href="/contact">Contact</a></li>
           </ul>*/}
-          <div className="ui container fluid" style={{height:'100vh'}}>
+          <div className="ui container fluid" style={{height:'100vh',paddingTop:'3rem'}}>
              <Route path="/login" component={LoginForm}/>
              <Route path="/home" component={Chat}/>
           </div>
